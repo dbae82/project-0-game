14f7e61d270965308d965a708fa76eee623c32c6
dbae82 <dbae82@gmail.com>
2021-07-14 16:54:33 -0500

    cleaned up repetitve javascript

--- scripts/app.js
+++ scripts/app.js
@@ -22,45 +22,58 @@ class Character {
     addStudy() {
         return this.study ++;
     };
+    zeroStat (num) {
+        if (num <= 0) {
+            clearInterval(this.timer);
+            $("#game-over-screen").css("display", "flex");
+        }else if (this.clock <= 0) {
+            clearInterval(this.timer);
+            $("#game-over-screen").css("display", "flex");
+            $("#message p").text("Congrats on retirement!!");    
+        };
+    }
     reduceStatusBar = () => {
         // console.log("type of health", typeof this.health);
         this.health -= 2;
         this.sleep -= 3;
         this.study -= 4;
-        this.clock -= 200;
+        this.clock -= 100;
         // console.log("health", this.health);
         // console.log("sleep", this.sleep);
         // console.log("study", this.study);
         $("#red").attr("value", `${player.health}`);
         $("#green").attr("value", `${player.sleep}`);
         $("#blue").attr("value", `${player.study}`);
-        if (this.health <= 0) {
-            // console.log("no more health");
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-        }else if (this.clock <= 0) {
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-            $("#message p").text("Congrats on retirement!!");    
-        };
-        if (this.sleep <= 0) {
-            // console.log("no more health");
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-        }else if (this.clock <= 0) {
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-            $("#message p").text("Congrats on retirement!!");    
-        };
-        if (this.study <= 0) {
-            // console.log("no more health");
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-        }else if (this.clock <= 0) {
-            clearInterval(this.timer);
-            $("#game-over-screen").css("display", "flex");
-            $("#message p").text("Congrats on retirement!!");    
-        };
+        // if (this.health <= 0) {
+        //     // console.log("no more health");
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        // }else if (this.clock <= 0) {
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        //     $("#message p").text("Congrats on retirement!!");    
+        // };
+        // if (this.sleep <= 0) {
+        //     // console.log("no more health");
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        // }else if (this.clock <= 0) {
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        //     $("#message p").text("Congrats on retirement!!");    
+        // };
+        // if (this.study <= 0) {
+        //     // console.log("no more health");
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        // }else if (this.clock <= 0) {
+        //     clearInterval(this.timer);
+        //     $("#game-over-screen").css("display", "flex");
+        //     $("#message p").text("Congrats on retirement!!");    
+        // };
+        this.zeroStat(this.health);
+        this.zeroStat(this.sleep);
+        this.zeroStat(this.study);
     };
     startTimer() {
         this.timer = setInterval(this.reduceStatusBar, 1000);
@@ -92,6 +105,7 @@ $("#red-btn").on("click", function(event) {
     // console.log(player);
     player.addHealth();
     $("#red").attr("value", `${player.health}`);
+    // $(".fa-plus-square").addclass("animate__bounceIn");
 });
 
 $("#green-btn").on("click", function(event) {
